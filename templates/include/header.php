6d2d1700366972f2fd63762600d010a327000230
Andrew <bibiw@localhost>
2017-03-12 19:12:14 +0300

    new file:                   admin.php 	new file:                   classes/Article.php 	new file:                   config.php 	new file:                   images/logo.png 	new file:                   index.php 	new file:                   style.css 	new file:                   templates/admin/editArticle.php 	new file:                   templates/admin/listArticles.php 	new file:                   templates/admin/loginForm.php 	new file:                   templates/archive.php 	new file:                   templates/homepage.php 	new file:                   templates/include/footer.php 	new file:                   templates/include/header.php 	new file:                   templates/viewArticle.php

---
+++ templates/include/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title><?php echo htmlspecialchars( $results['pageTitle'] )?></title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+  </head>
+  <body>
+    <div id="container">
+      <h1>Very good site</h1>
+ 
+      <a href="."><img style="left: : 100px;" id="logo" src="images/logo.png" alt="CMS" /></a>
